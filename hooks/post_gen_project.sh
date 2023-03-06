f502c499604669a729630e80b80633d5477784f1
Michael Ford <mford@sfu.ca>
2023-03-06 16:06:30 -0500

    Added subdir creation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,2 +1,9 @@
 #! /usr/bin/env bash
 ln -s ../../data ./
+
+add_locus_subdir="{{ cookiecutter.add_locus_subdir }}"
+add_locus_subdir_option=${add_locus_subdir:0:1}
+if [[ "$add_locus_subdir_option" == "I" ]]; then
+mkdir -p notebooks/{IGHV,TRAV,IGLV,IGKV}
+sed -i "s/os.chdir('\.\.\/')/os.chdir('\.\.\/\.\.\/')/" notebooks/template.ipynb
+fi
